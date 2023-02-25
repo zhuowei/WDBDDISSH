c8a4d916b931194fc7300e61d6831f530dcf2eec
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2023-02-25 12:31:45 -0500

    more cert

--- gencert.sh
+++ gencert.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-openssl req -x509 -newkey rsa:1024 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes -subj "/C=CA/CN=Worth Doing Badly DDI"
+set -e
+# macOS's LibreSSL doesn't come with a openssl.crf that supports the ca command
+# you could copy the config file, or you could just use Homebrew's OpenSSL
+OPENSSL="/usr/local/Cellar/openssl@3/3.0.7/bin/openssl"
+rm -r demoCA || true
+mkdir -p demoCA/newcerts || true
+echo 01 > demoCA/serial
+touch demoCA/index.txt
+"$OPENSSL" req -newkey rsa:1024 -keyout key.pem -out req.pem -sha256 -nodes -subj "/C=CA/ST=BC/O=Worth Doing Badly/CN=Worth Doing Badly DDI"
+"$OPENSSL" ca -batch -selfsign -keyfile key.pem -in req.pem -out cert.pem -startdate 070416225531Z -enddate 140416225531Z

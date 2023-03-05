09a7b0df7f2913810a7a95d068bca4c4cbb1129d
verygenericname <dedbeddedbed@gmail.com>
2023-03-05 15:16:43 -0500

    add workflows

--- patchddi.sh
+++ patchddi.sh
@@ -2,8 +2,9 @@
 # run as root
 set -e
 rm DeveloperDiskImageModified.dmg || true
-hdiutil convert -format UDRW -o DeveloperDiskImageModified.dmg /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/16.1/DeveloperDiskImage.dmg
-hdiutil attach -owners on DeveloperDiskImageModified.dmg
+hdiutil convert -format UDRW -o DeveloperDiskImageModified_16.1.dmg /Applications/Xcode_14.2.app/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/16.1/DeveloperDiskImage.dmg
+hdiutil convert -format UDRW -o DeveloperDiskImageModified_16.0.dmg /Applications/Xcode_14.2.app/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/16.0/DeveloperDiskImage.dmg
+hdiutil attach -owners on DeveloperDiskImageModified_16.1.dmg
 for i in com.apple.debugserver.plist
 do
 	cp $i /Volumes/DeveloperDiskImage/Library/LaunchDaemons/
@@ -14,6 +15,21 @@ for i in com.apple.ps.plist
 do
         ln -s ../../../bin/ps /Volumes/DeveloperDiskImage/usr/bin/
 	cp $i /Volumes/DeveloperDiskImage/Library/LaunchDaemons/
+        chown root:wheel /Volumes/DeveloperDiskImage/Library/LaunchDaemons/$i
+        chmod 644 /Volumes/DeveloperDiskImage/Library/LaunchDaemons/$i
+done
+hdiutil detach /Volumes/DeveloperDiskImage
+hdiutil attach -owners on DeveloperDiskImageModified_16.0.dmg
+for i in com.apple.debugserver.plist
+do
+        cp $i /Volumes/DeveloperDiskImage/Library/LaunchDaemons/
+        chown root:wheel /Volumes/DeveloperDiskImage/Library/LaunchDaemons/$i
+        chmod 644 /Volumes/DeveloperDiskImage/Library/LaunchDaemons/$i
+done
+for i in com.apple.ps.plist
+do
+        ln -s ../../../bin/ps /Volumes/DeveloperDiskImage/usr/bin/
+        cp $i /Volumes/DeveloperDiskImage/Library/LaunchDaemons/
         chown root:wheel /Volumes/DeveloperDiskImage/Library/LaunchDaemons/$i
         chmod 644 /Volumes/DeveloperDiskImage/Library/LaunchDaemons/$i
 done
--- signddi.sh
+++ signddi.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-openssl dgst -sign key.pem -out DeveloperDiskImageModified.dmg.signature -binary -sha1 DeveloperDiskImageModified.dmg
+openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.1.dmg.signature -binary -sha1 DeveloperDiskImageModified_16.1.dmg
+openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.0.dmg.signature -binary -sha1 DeveloperDiskImageModified_16.0.dmg

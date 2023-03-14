b50873807eed1a3968bdc9baf01f6f0c06d15a48
Nathan <87825638+verygenericname@users.noreply.github.com>
2023-03-13 20:55:53 -0400

    support for 15.5-15.7

--- signddi.sh
+++ signddi.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.1.dmg.signature -binary -sha1 DeveloperDiskImageModified_16.1.dmg
 openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.0.dmg.signature -binary -sha1 DeveloperDiskImageModified_16.0.dmg
+openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.0.dmg.signature -binary -sha1 DeveloperDiskImageModified_15.7.dmg
+openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.0.dmg.signature -binary -sha1 DeveloperDiskImageModified_15.6.dmg
+openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.0.dmg.signature -binary -sha1 DeveloperDiskImageModified_15.5.dmg
